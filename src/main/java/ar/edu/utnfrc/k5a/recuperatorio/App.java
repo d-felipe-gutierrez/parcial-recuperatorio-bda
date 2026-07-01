73b7b909099a2e0fd6c84caf027689f672f2acc5
Diego Felipe Gutierrez <dfgutierrez2004@outlook.com>
2026-07-01 02:18:23 -0300

    tp-recuperatorio v1.0

---
+++ src/main/java/ar/edu/utnfrc/k5a/recuperatorio/App.java
@@ -0,0 +1,10 @@
+package ar.edu.utnfrc.k5a.recuperatorio;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
